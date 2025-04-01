4a0abbfa4af2f0a5a0aadad683b6502ba0054d05
Jashanfzk <125443809+Jashanfzk@users.noreply.github.com>
2025-04-01 17:13:37 -0400

    Final version

---
+++ Cumulative1/Controllers/TeacherAPIController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Cumulative1.Models;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+namespace Cumulative1.Controllers
+{
+    [Route("api/Teacher")]
+    [ApiController]
+    public class TeacherAPIController : ControllerBase
+    {
+        private readonly SchoolDbContext _context;
+        // Dependency injection of database context
+        public TeacherAPIController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+        /// <summary>
+        /// Returns a list of all teachers in the system.
+        /// </summary>
+        /// <example>
+        /// GET api/Teacher/TeacherNames -> [{"TeacherId":2, "TeacherFName":"Caitlin", "TeacherLName":"Cummings", "TeacherEmployeeID":"IdT381", "HireDate":"10-06-2014 00:00:00", "Salary":62.77}, ...]
+        /// </example>
+        /// <returns>
+        /// A list of Teacher objects containing details of all teachers.
+        /// </returns>
+        [HttpGet]
+        [Route(template: "TeacherNames")]
+        public List<Teacher> TeacherNameList()
+        {
+            // Create an empty list to store teacher details
+            List<Teacher> teacherDetails = new List<Teacher>();
+            // Connection is being assigned to the connection string
+            MySqlConnection Connection = _context.AccessDataBase();
+
+            Connection.Open();
+            string SQLquery = "SELECT * FROM teachers";
+            // Create a SQL command to retrieve all teachers
+            MySqlCommand Command = Connection.CreateCommand();
+
+            Command.CommandText = SQLquery;
+
+            // Execute the query and store the result set
+            MySqlDataReader ResultSet = Command.ExecuteReader();
+            // Loop through each row in the result set
+            while (ResultSet.Read())
+            {
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string TeacherFName = ResultSet["teacherfname"].ToString();
+                string TeacherLName = ResultSet["teacherlname"].ToString();
+                string TeacherEmployeeID = ResultSet["employeenumber"].ToString();
+                DateTime HireDate = Convert.ToDateTime(ResultSet["hiredate"]);
+                double Salary = Convert.ToDouble(ResultSet["salary"]);
+
+
+
+                
+                Teacher newteacher = new Teacher();
+
+                newteacher.TeacherFName = TeacherFName;
+                newteacher.TeacherLName = TeacherLName;
+                newteacher.TeacherEmployeeID = TeacherEmployeeID;
+                newteacher.HireDate = HireDate;
+                newteacher.Salary = Salary;
+                newteacher.TeacherId = Convert.ToInt32(ResultSet["TeacherId"]);
+                // Add the teacher object to the list
+                teacherDetails.Add(newteacher);
+            }
+            Connection.Close();
+            return teacherDetails;     // Return the final list of teacher details
+
+        }
+        /// <summary>
+        /// Retrieves a specific teacher's details by their ID.
+        /// </summary>
+        /// <param name="id">The unique identifier of the teacher.</param>
+        /// <returns>
+        /// A Teacher object containing details all the teachers.
+        /// </returns>
+        /// <example>
+        /// GET api/Teacher/FindTeacherById/1 -> {"TeacherId":3, "TeacherFName":"Linda", "TeacherLName":"Chan", "TeacherEmployeeID":"EMP001", "HireDate":"22-08-2015 00:00:00", "Salary":60.22}
+        /// In this we can get a list of teachers on a sam page and by 
+        /// </example>
+        [HttpGet]
+        [Route(template: "FindTeacherById/{id}")]
+        public Teacher FindTeacherById(int id)
+        {
+
+            // Connection is being assigned to the connection string
+            MySqlConnection Connection = _context.AccessDataBase();
+
+            Connection.Open();
+
+            string SQLquery = "SELECT * FROM teachers WHERE teacherid =" + id.ToString();
+            // Create SQL query to find teacher by ID
+            MySqlCommand Command = Connection.CreateCommand();
+
+            Command.CommandText = SQLquery;
+
+            MySqlDataReader ResultSet = Command.ExecuteReader();
+
+            Teacher newteacher = new Teacher();
+            // Loop through the result set
+            while (ResultSet.Read())
+            {
+                int TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+                string TeacherFName = ResultSet["teacherfname"].ToString();
+                string TeacherLName = ResultSet["teacherlname"].ToString();
+                string TeacherEmployeeID = ResultSet["employeenumber"].ToString();
+                DateTime HireDate = Convert.ToDateTime(ResultSet["hiredate"]);
+                double Salary = Convert.ToDouble(ResultSet["salary"]);
+
+
+
+                newteacher.TeacherFName = TeacherFName;
+                newteacher.TeacherLName = TeacherLName;
+                newteacher.TeacherEmployeeID = TeacherEmployeeID;
+                newteacher.HireDate = HireDate;
+                newteacher.Salary = Salary;
+                newteacher.TeacherId = Convert.ToInt32(ResultSet["teacherid"]);
+            }
+            Connection.Close();
+            // Return the teacher object with the retrieved details
+            return newteacher;
+        }
+
+    }
+}
+
---
+++ Cumulative1/Controllers/TeacherPageController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cumulative1.Models;
+namespace Cumulative1.Controllers
+{
+    public class TeacherPageController : Controller
+    {
+        //This is a private variable that holds the connection of data base
+        private readonly TeacherAPIController _api;
+        //This is a constructor that initializes the connection of data base
+        public TeacherPageController(TeacherAPIController api)
+        {
+            //when this method is used we want to see the list of teachers 
+            _api = api;
+        }
+        /// <summary>
+        /// Retrieves a list of all teachers.
+        /// </summary>
+        /// <returns>A view displaying the list of teachers.</returns>
+        /// <example>
+        /// GET /TeacherPage/List
+        /// </example>
+        public IActionResult List()
+        {
+            List<Teacher> teacherList = _api.TeacherNameList();
+            return View(teacherList);
+        }
+        /// <summary>
+        /// Retrieves details of a specific teacher based on their ID.
+        /// </summary>
+        /// <param name="Id">The unique identifier of the teacher.</param>
+        /// <returns>A view displaying the teacher's details.</returns>
+        /// <example>
+        /// GET /TeacherPage/Show/1
+        /// </example>
+        public IActionResult Show(int Id)
+        {
+            Teacher teacherlist1 = _api.FindTeacherById(Id);
+            return View(teacherlist1);
+        }
+    }
+}
